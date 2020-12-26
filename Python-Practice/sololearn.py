6f0645298e3c32127b4315a85f0809fa41886be1
Trey McGarity <trey.mcgarity99@gmail.com>
2020-12-26 00:19:07 -0600

    average word length problem

--- Python-Practice/sololearn.py
+++ Python-Practice/sololearn.py
@@ -1,7 +1,21 @@
+import math
 
 #print reverse string
-string = "hello"
+string = "hello world"
 def reverse_string(text):
     return text[::-1]
     
 print(reverse_string(string))
+
+def averageLength(text):
+    cleanup  = string.translate({ord(i): None for i in ".,;!?"})
+    count = 0
+    words = cleanup.split()
+
+    for word in words:
+        for letters in word:
+            count += 1
+
+    return math.ceil(count / len(words))
+
+print(averageLength(string))
